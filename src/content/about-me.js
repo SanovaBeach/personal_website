a734a74f893ef6f6f78d44346fe64d223e527a89
SanovaBeach <incredible.commanders@gmail.com>
2021-07-24 23:20:38 +0800

    finished about

--- src/content/about-me.js
+++ src/content/about-me.js
@@ -1,30 +1,20 @@
 export const about = [
   {
-    input: "Justin.currentLocation",
-    return: '"San Jose, CA"',
+    input: "Laurence.currentLocation",
+    return: '"Bulacan, PH"',
   },
   {
-    input: "Justin.contactInfo",
+    input: "Laurence.contactInfo",
     return:
-      '["<a href="mailto:laurencebalazo@gmail.com">laurencebalazo@gmail.com</a>", "<a rel="noopener" href="https://www.linkedin.com/in/justin-chi-64b12b37">LinkedIn</a>", "<a rel="noopener" href="https://github.com/vai0">github</a>"]',
+      '["<a target="_blank" href="mailto:laurencebalazo@gmail.com">laurencebalazo@gmail.com</a>", "<a rel="noopener" target="_blank" href="https://www.linkedin.com/in/laurence-balazo-51a782216/">LinkedIn</a>", "<a rel="noopener" target="_blank" href="https://github.com/SanovaBeach">Github</a>]',
   },
   {
-    input: "Justin.resume",
-    return:
-      '"<a rel="noopener" href="/justinchi_resume.pdf" target="_blank">justinchi.pdf</a>"',
+    input: "Laurence.interests",
+    return: '["Reading", "Watching", "Video Games", "Jogging"]',
   },
+
   {
-    input: "Justin.interests",
-    return: '["design", "basketball", "cooking", "starcraft"]',
-  },
-  {
-    input: "Justin.education",
-    return:
-      '"B.Sc. Structural Engineering - University of California, San Diego"',
-  },
-  {
-    input: "Justin.skills",
-    return:
-      '["Sass", "JavaScript", "Python", "React", "Redux", "Flask", "webpack", "git"]',
+    input: "Laurence.skills",
+    return: '["Sass", "JavaScript",  "React", "git"]',
   },
 ];
